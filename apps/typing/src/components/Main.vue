f81a377d0c520c87bd4f46d2e35628b1f8d53205
Armand Philippot <git@armandphilippot.com>
2022-03-17 14:31:43 +0100

    chore(typing): add a start button

--- apps/typing/src/components/Main.vue
+++ apps/typing/src/components/Main.vue
@@ -1,20 +1,33 @@
 <template>
     <main id="main">
         <Instructions />
-        <TypingForm />
+        <StartBtn v-if="!shouldStart" @click="toggleForm" />
+        <TypingForm v-if="shouldStart" />
     </main>
 </template>
 
 <script>
 import Instructions from "./Instructions.vue";
 import TypingForm from "./TypingForm.vue";
+import StartBtn from "./StartBtn.vue";
 
 export default {
     name: 'Main',
     components: {
         Instructions,
-        TypingForm
+        TypingForm,
+        StartBtn
     },
+    data() {
+        return {
+            shouldStart: false
+        }
+    },
+    methods: {
+        toggleForm() {
+            this.shouldStart = true;
+        }
+    }
 }
 </script>
 
---
+++ apps/typing/src/components/StartBtn.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="btn">Start</button>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.btn {
+    display: block;
+    margin: 2rem auto;
+    padding: 0.5rem 1rem;
+    background: hsl(0, 0%, 100%);
+    border: solid 2px hsl(216, 57%, 27%);
+    border-radius: 5px;
+    font-size: 1rem;
+    font-weight: 600;
+    cursor: pointer;
+}
+
+.btn:hover, .btn:focus {
+    background: hsl(0, 0%, 90%);
+}
+
+.btn:active {
+    background: hsl(0, 0%, 95%);
+}
+</style>
